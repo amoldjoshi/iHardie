a689fe88c85a50cd24f3694cff5212c878bbe44f
Amol D. Joshi <amoldjoshi@gmail.com>
2014-01-21 10:59:37 -0800

    Creating initial framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IHardie::Application.config.secret_token = '7a48908770682a3d0b7551fec8dfc123a0cf64ef20a5fffebc39f1254abcb8c0722ed2f95b7e7283b80a05beba3ef6ae741a9572f3c2b00dded7796b34f3ac8a'
